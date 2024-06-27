cb26a594c0ff2f94b52713189a929342ab34529f
Harshavardhan kale <94823507+kaleharshavardhan07@users.noreply.github.com>
2024-06-27 15:25:59 +0530

    Time: 26 ms (40.86%), Space: 22.8 MB (41.42%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+   static bool cmp(const vector<int>& a, const vector<int>& b) { return a[0] < b[0]; }
+
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>>ret;
+        sort(intervals.begin(), intervals.end(), cmp);
+        int i = 1;
+        ret.push_back(intervals[0]);
+        while (i < intervals.size()) {
+
+            if (intervals[i][0] <= ret.back()[1]) {
+                ret.back()[1] = max(intervals[i][1], ret.back()[1]);
+
+            } else {
+                ret.push_back(intervals[i]);
+            }
+            i++;
+        }
+        return ret;
+    }
+};
