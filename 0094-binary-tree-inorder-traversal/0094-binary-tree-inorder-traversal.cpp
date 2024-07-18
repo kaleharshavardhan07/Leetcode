76aa657987cd0734bbde6e40cccd3e5aa2a37df7
Harshavardhan_kale <94823507+kaleharshavardhan07@users.noreply.github.com>
2024-07-18 09:19:36 +0530

    Time: 0 ms (100%), Space: 10.2 MB (11.13%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -6,24 +6,47 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-     void inorder(TreeNode* root, vector<int> &arr){
-        
-        if(root == nullptr){
-            return;
-        }
-        
-        inorder(root->left, arr);
-        arr.push_back(root->val);
-        inorder(root->right, arr);
-    }
+    //  void inorder(TreeNode* root, vector<int> &arr){
+
+    //     if(root == nullptr){
+    //         return;
+    //     }
+
+    //     inorder(root->left, arr);
+    //     arr.push_back(root->val);
+    //     inorder(root->right, arr);
+    // }
+    // vector<int> inorderTraversal(TreeNode* root) {
+    //    vector<int> arr;
+    //     inorder(root, arr);
+    //     return arr;
+    // }
     vector<int> inorderTraversal(TreeNode* root) {
-       vector<int> arr;
-        inorder(root, arr);
-        return arr; 
+        vector<int> ret;
+
+        stack<TreeNode*> st;
+        TreeNode* node = root;
+        while (true) {
+            if (node != NULL) {
+                st.push(node);
+                node = node->left;
+            } else {
+                if (st.empty())
+                    break;
+                else {
+                    node = st.top();
+                    st.pop();
+                    ret.push_back(node->val);
+                    node = node->right;
+                }
+            }
+        }
+        return ret;
     }
 };
