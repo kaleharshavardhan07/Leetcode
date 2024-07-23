b27c1d2505e0d498857b5a0aec363a0ed1a64321
Harshavardhan_kale <94823507+kaleharshavardhan07@users.noreply.github.com>
2024-07-23 22:37:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        if(s.length()==0){
+            return true;
+        }
+        if(s.length()>t.length())return false;
+        
+      int i=0; int j=0;
+      while(i<s.length() && j<t.length()){
+        if(s[i]==t[j]){
+            i++;j++;
+            if(i==s.length()-1){return true;}
+        }
+        else{j++;}
+
+      }  
+      return false;
+    }
+};
