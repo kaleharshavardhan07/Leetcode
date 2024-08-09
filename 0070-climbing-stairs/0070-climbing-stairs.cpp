c8d93cf1c0ee5ee7b50ae1e3a7810fefda2f207c
Harshavardhan kale <94823507+kaleharshavardhan07@users.noreply.github.com>
2024-08-09 21:31:27 +0530

    Time: 0 ms (100%), Space: 8.3 MB (6.37%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,23 +1,23 @@
+#include <unordered_map>
+
 class Solution {
+private:
+    std::unordered_map<int, int> memo;
+    
+    int solve(int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        
+        if (memo.find(n) != memo.end()) {
+            return memo[n];
+        }
+        
+        memo[n] = solve(n - 1) + solve(n - 2);
+        return memo[n];
+    }
+    
 public:
     int climbStairs(int n) {
-        if(n==1){
-            return 1;
-        }
-        else if (n==2){
-            return 2;
-        }
-        else{
-            int d[n+1];
-            d[1]=1;
-            d[2]=2;
-            for(int i=3;i<=n;i++){
-                d[i]=d[i-1]+d[i-2];
-                
-
-
-            }
-            return d[n];
-        }
+        return solve(n);
     }
 };
