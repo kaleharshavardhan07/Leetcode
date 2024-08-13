82fe881e91a391c976ba682080991ea9de321d23
Harshavardhan kale <94823507+kaleharshavardhan07@users.noreply.github.com>
2024-08-13 10:59:17 +0530

    Time: 38 ms (21.08%), Space: 25.4 MB (26.63%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        int c = 0;
+        map<string, vector<string>> mapi;
+        vector<vector<string>> ret;
+        for (string s : strs) {
+            string r = s;
+            sort(r.begin(), r.end());
+            // auto it = mapi.find(r);
+            if (mapi.find(r) != mapi.end()) {
+                mapi[r].push_back(s);
+
+            } else {
+                mapi[r] = {s};
+            }
+        }
+        for(auto it :mapi){
+            ret.push_back(it.second);
+        }
+        return ret;
+    }
+};
