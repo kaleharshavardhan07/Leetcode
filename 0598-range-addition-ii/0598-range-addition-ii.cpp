1c30c59af1e57df1d3490ea1878fe337c9dce6f5
Harshavardhan kale <94823507+kaleharshavardhan07@users.noreply.github.com>
2024-08-20 18:45:19 +0530

    Time: 14 ms (6.26%), Space: 14.9 MB (9.24%) - LeetHub

--- 0598-range-addition-ii/0598-range-addition-ii.cpp
+++ 0598-range-addition-ii/0598-range-addition-ii.cpp
@@ -2,17 +2,16 @@ class Solution {
 public:
     int maxCount(int m, int n, vector<vector<int>>& ops) {
         
-        int minr=m;
-        int mincol=n;
+       
         for(auto it :ops){
             if(it[0]>0 && it[1]>0){
               
-                minr=min(minr,it[0]);
-                mincol=min(mincol,it[1]);
+                m=min(m,it[0]);
+                n=min(n,it[1]);
 
             }
         }
        
-        return minr*mincol;
+        return m*n;
     }
 };
