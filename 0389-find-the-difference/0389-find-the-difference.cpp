e94bb13379724c241e17fbd48a75afdb305a2912
Harshavardhan kale <94823507+kaleharshavardhan07@users.noreply.github.com>
2024-08-21 00:28:50 +0530

    Time: 4 ms (50.41%), Space: 8.1 MB (46.59%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        vector<int>freq(26,0);
+        for( char c: t){
+            freq[c-'a']++;
+        }
+        for(char c:s){
+            freq[c-'a']--;
+        }
+        for(int i=0;i<26;i++){
+            if(freq[i]==1){
+                return static_cast<char>(i+'a');
+            }
+        }
+        return 'l';
+    }
+};
