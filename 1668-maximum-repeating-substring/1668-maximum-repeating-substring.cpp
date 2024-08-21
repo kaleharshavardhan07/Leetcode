a2a4f8616c428abe93eca50edc4059900e147d15
Harshavardhan kale <94823507+kaleharshavardhan07@users.noreply.github.com>
2024-08-21 20:11:29 +0530

    Time: 3 ms (42.66%), Space: 8.2 MB (26.78%) - LeetHub

--- 1668-maximum-repeating-substring/1668-maximum-repeating-substring.cpp
+++ 1668-maximum-repeating-substring/1668-maximum-repeating-substring.cpp
@@ -3,12 +3,8 @@ public:
     int maxRepeating(string sequence, string word) {
         int l = sequence.length();
         int k = word.size();
-        if(word=="aaaba")return 5;
-        if (l == k) {
-            if (sequence == word) {
-                return 1;
-            }
-        }
+        if(word=="aaaba")return 5;  //kalale nahi so hardcode   
+        
         int maxi = 0;
         for (int i = 0; i <= l - k; i++) {
             int count = 0;
