41035157044e26683b2b9c9f6ce1960fd5b61b5b
Harshavardhan kale <94823507+kaleharshavardhan07@users.noreply.github.com>
2024-08-26 12:02:11 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+void solve(string s, int i,int a, int b, int c, int&count){
+    if(a>0 && b>0 && c>0)count++;
+
+
+    
+    
+}
+    int numberOfSubstrings(string s) {
+        solve(s,0,0,0,count);
+    }
+};
