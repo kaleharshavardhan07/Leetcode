3599bb96d171c10dbc0205b8672141e9548212b7
Harshavardhan kale <94823507+kaleharshavardhan07@users.noreply.github.com>
2024-09-03 10:54:26 +0530

    Time: 548 ms (5.06%), Space: 22.2 MB (12.95%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,22 +1,31 @@
 class Solution {
 public:
-    void solve(int amount, vector<int>& coins, int& count, int curr) {
+    void solve(int amount, vector<int>& coins, int& count, int curr, vector<vector<int>>& memo) {
         if (amount == 0) {
             count++;
-            return ;
+            return;
         }
-        if (amount < 0)
-            return ;
+        if (amount < 0) {
+            return;
+        }
+        if (memo[curr][amount] != -1) {
+            count += memo[curr][amount];
+            return;
+        }
 
-        for (int i=curr;i<coins.size();i++) {
-            solve(amount - coins[i], coins, count,i);
+        int ways = 0;
+        for (int i = curr; i < coins.size(); i++) {
+            solve(amount - coins[i], coins, ways, i, memo);
         }
+
+        memo[curr][amount] = ways;
+        count += ways;
     }
+
     int change(int amount, vector<int>& coins) {
-        sort(coins.begin(),coins.end());
-        vector<int> memo(amount + 1, -1);
+        vector<vector<int>> memo(coins.size(), vector<int>(amount + 1, -1));
         int count = 0;
-        solve(amount, coins, count,0);
+        solve(amount, coins, count, 0, memo);
         return count;
     }
 };
