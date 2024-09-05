a0777dc38c13122a3b3095440817fa9c634f069b
Harshavardhan kale <94823507+kaleharshavardhan07@users.noreply.github.com>
2024-09-06 00:11:18 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        while (num > 10) {
+            int temp = num;
+            int k=0;
+            while (temp > 0) {
+                k = k + temp % 10;
+                temp = temp / 10;
+            }
+            num = k;
+        }
+        return num;
+    }
+};
