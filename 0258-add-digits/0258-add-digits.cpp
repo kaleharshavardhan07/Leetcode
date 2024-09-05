19a8187a83aeb7f75ee42b659de48a1f8b1fba04
Harshavardhan kale <94823507+kaleharshavardhan07@users.noreply.github.com>
2024-09-06 00:11:21 +0530

    Time: 5 ms (18.91%), Space: 8.4 MB (13.02%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     int addDigits(int num) {
-        while (num > 10) {
+        while (num >=10) {
             int temp = num;
             int k=0;
+
             while (temp > 0) {
                 k = k + temp % 10;
                 temp = temp / 10;
