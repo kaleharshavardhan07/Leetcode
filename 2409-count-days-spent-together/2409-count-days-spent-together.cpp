0b926d9fc587f15baa8c7e018a1f07092ddee0a6
Harshavardhan kale <94823507+kaleharshavardhan07@users.noreply.github.com>
2024-09-25 22:19:02 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2409-count-days-spent-together/2409-count-days-spent-together.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int countDaysTogether(string arriveAlice, string leaveAlice, string arriveBob, string leaveBob) {
+        
+        string am=arriveAlice.substr(0,2);
+        string am=arriveAlice.substr(0,2);
+        string am=arriveAlice.substr(0,2);
+    }
+};
