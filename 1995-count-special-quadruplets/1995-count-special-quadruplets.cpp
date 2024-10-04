619c6e142f471dab649d6dd3de3cd3d82210ccc0
Harshavardhan kale <94823507+kaleharshavardhan07@users.noreply.github.com>
2024-10-04 10:19:02 +0530

    Time: 23 ms (17.29%), Space: 12.9 MB (95.11%) - LeetHub

--- 1995-count-special-quadruplets/1995-count-special-quadruplets.cpp
+++ 1995-count-special-quadruplets/1995-count-special-quadruplets.cpp
@@ -1,29 +1,17 @@
 class Solution {
 public:
     int countQuadruplets(vector<int>& nums) {
-        int count = 0;
-        int n = nums.size();
-        
-        // Iterate over possible values for d (we start with the largest value of d)
-        for (int d = n - 1; d >= 3; --d) {
-            // Map to track sums of triplets (a + b + c)
-            unordered_map<int, int> sumMap;
-            
-            // Iterate over possible values for c
-            for (int c = d - 1; c >= 2; --c) {
-                // Iterate over possible values for b
-                for (int b = c - 1; b >= 1; --b) {
-                    // Iterate over possible values for a
-                    for (int a = b - 1; a >= 0; --a) {
-                        // If the sum of nums[a] + nums[b] + nums[c] equals nums[d], we found a quadruplet
-                        if (nums[a] + nums[b] + nums[c] == nums[d]) {
-                            count++;
-                        }
+        int count=0;
+        int n=nums.size();
+        for(int a=0;a<n;a++){
+            for(int b=a+1;b<n;b++){
+                for(int c=b+1;c<n;c++){
+                    for(int d=c+1;d<n;d++){
+                        if(nums[a]+nums[b]+nums[c]==nums[d])count++;
                     }
                 }
             }
         }
-        
         return count;
     }
 };
