32919f9d0de0498535edda209caf1686ecd67e6c
Harshavardhan kale <94823507+kaleharshavardhan07@users.noreply.github.com>
2024-10-19 10:44:06 +0530

    Time: 2 ms (81.82%), Space: 5.4 MB (100%) - LeetHub

---
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.c
@@ -0,0 +1,19 @@
+char findKthBit(int n, int k) {
+    if (n == 1) {
+        return '0';
+    }
+    
+    int length = pow(2, n) - 1;
+    int mid = length / 2 + 1;
+    
+    if (k == mid) {
+        return '1';
+    }
+    
+    if (k < mid) {
+        return findKthBit(n - 1, k);
+    } else {
+        char invert_bit = findKthBit(n - 1, length - k + 1);
+        return (invert_bit == '0') ? '1' : '0';
+    }
+}
