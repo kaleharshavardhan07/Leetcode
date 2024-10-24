fae86da9522e9360c9ad1ba14a3e11d28b593419
Harshavardhan kale <94823507+kaleharshavardhan07@users.noreply.github.com>
2024-10-24 13:21:31 +0530

    Time: 7 ms (17.1%), Space: 9.7 MB (36.85%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
-    vector<int> countBits(int n) {
-        
+int count(int k){
+        int c=0;
+        while(k!=0){
+            if(k%2!=0){
+                c++;
+            }
+            k=k/2;
+        }
+        return c;
     }
+    vector<int> countBits(int n) {
+        vector<int> arr;
+        for(int i=0;i<=n;i++){
+              int j=count(i);
+              arr.push_back(j);
+        }
+        return arr;
+    }
+    
 };
