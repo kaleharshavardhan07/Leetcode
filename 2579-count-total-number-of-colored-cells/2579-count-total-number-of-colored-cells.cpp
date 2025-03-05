29187fa08881f69f8e52afb3a7db615e2eb199fd
Harshavardhan kale <94823507+kaleharshavardhan07@users.noreply.github.com>
2025-03-05 09:46:44 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.cpp
+++ 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.cpp
@@ -1,13 +1,19 @@
 class Solution {
 public:
-    long long coloredCells(int n) {
+    // long long coloredCells(int n) {
 
-        if( n==1){
-            return 1;
+    //     if( n==1){
+    //         return 1;
 
-        }
+    //     }
         
-        return 4+4*(n-2)+coloredCells(n-1);
+    //     return 4+4*(n-2)+coloredCells(n-1);
         
-    }
+    // }
+
+     long long coloredCells(int n){
+unsigned long long n = _n;
+        return 1 + 2 * (n-1) * (n);     }
+
+
 };
