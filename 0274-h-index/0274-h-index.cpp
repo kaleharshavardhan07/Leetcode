72a0425bd1472dc2dbfc50947b811768fbac414a
Harshavardhan kale <94823507+kaleharshavardhan07@users.noreply.github.com>
2025-08-08 12:03:15 +0530

    Time: 0 ms (100%), Space: 12.3 MB (63.4%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -4,18 +4,18 @@ public:
         sort(citations.begin(), citations.end());
         
         int n = citations.size();
-        int hIndex = 0; 
+        int ind = 0; 
          for (int i = 0; i < n; i++) {
            
             int h = n - i;
 
             
             if (citations[i] >= h) {
-                hIndex = h;
+                ind = h;
                 break;
             }
         }
         
-        return hIndex;
+        return ind;
     }
 };
