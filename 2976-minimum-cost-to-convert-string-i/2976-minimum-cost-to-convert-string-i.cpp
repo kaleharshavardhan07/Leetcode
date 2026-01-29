3cce7a04fa8ce2b2962086f80811e763eebc94da
Harshavardhan kale <94823507+kaleharshavardhan07@users.noreply.github.com>
2026-01-29 20:06:40 +0530

    Time: 30 ms (71.59%), Space: 97.7 MB (92.84%) - LeetHub

--- 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.cpp
+++ 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.cpp
@@ -1,7 +1,13 @@
+#include <vector>
+#include <string>
+#include <climits>
+
+using namespace std;
+
 class Solution {
 public:
-    void proc(vector < vector<int>>&mini) {
-       int  n = 26;
+    void proc(vector<vector<int>>& mini) {
+        int n = 26;  
         for (int k = 0; k < n; ++k) {
             for (int i = 0; i < n; ++i) {
                 for (int j = 0; j < n; ++j) {
@@ -12,14 +18,30 @@ public:
             }
         }
     }
-    long long minimumCost(string source, string target, vector<char>& original,vector<char>& changed, vector<int>& cost) {
-        vector <vector<int>>mini(26,vector<int>(26, INT_MAX));
-        for (int i = 0; i < original.size(); i++) {
-            mini[original[i] - 'a'][changed[i] - 'a'] = cost[i];
+
+    long long minimumCost(string source, string target, vector<char>& original, vector<char>& changed, vector<int>& cost) {
+        if (source.length() != target.length()) {
+            return -1;
         }
+
+       
+        vector<vector<int>> mini(26, vector<int>(26, INT_MAX));
+   
+     
+        for (int i = 0; i < 26; ++i) {
+            mini[i][i] = 0;
+        }
+
+      
+        for (int i = 0; i < original.size(); ++i) {
+            mini[original[i] - 'a'][changed[i] - 'a'] = min(cost[i],mini[original[i] - 'a'][changed[i] - 'a']);
+        }
+
+      
         proc(mini);
+
         long long count = 0;
-        for (int i = 0; i < source.length(); i++) {
+        for (int i = 0; i < source.length(); ++i) {
             if (source[i] != target[i]) {
                 long long k = mini[source[i] - 'a'][target[i] - 'a'];
                 if (k != INT_MAX) {
@@ -29,6 +51,7 @@ public:
                 }
             }
         }
+
         return count;
     }
 };
