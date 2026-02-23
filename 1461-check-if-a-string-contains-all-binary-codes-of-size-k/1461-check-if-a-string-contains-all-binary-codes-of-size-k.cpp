dde221a3f04455ba55a38ee77721dd812dd5d0fd
Harshavardhan kale <94823507+kaleharshavardhan07@users.noreply.github.com>
2026-02-23 20:06:33 +0530

    Time: 1089 ms (10.86%), Space: 158 MB (14.31%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -1,29 +1,13 @@
 class Solution {
 public:
- std::vector<std::string> printAllBinaryNumbers(int k) {
-    int totalNumbers = 1 << k;
-    
-    std::vector<std::string> ret;
-    for (int i = 0; i < totalNumbers; ++i) {
-        std::bitset<64> binary(i); 
-        std::string binaryString = binary.to_string().substr(64 - k); 
-        ret.push_back(binaryString);
-    }
-    return ret;
-}
     bool hasAllCodes(string s, int k) {
-        vector<string> ret=  printAllBinaryNumbers( k);
-        map<string,int>mapi;
-        for(int i=0;i<s.size()-k;i++){
-            mapi[s.substr(i,k)]++;
-
+        set<string>st;
+        if( s.length()<k)return false;
+        for( int i=0;i< s.length()-k+1;i++){
+            string m= s.substr(i,k);
+            st.insert(m);
         }
-        for(string c : ret){
-            if(mapi[c]==0){
-                return false;
-            }
-        }
-        return true;
-        
+        if( pow(2,k) == st.size())return true;
+        return false;
     }
 };
