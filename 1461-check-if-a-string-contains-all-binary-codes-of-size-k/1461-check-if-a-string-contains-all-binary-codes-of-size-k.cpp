1d3ea06af4aa416679674159c6ab4f3456c004c1
Harshavardhan kale <94823507+kaleharshavardhan07@users.noreply.github.com>
2026-02-23 20:07:37 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
     bool hasAllCodes(string s, int k) {
-        set<string>st;
-        if( s.length()<k)return false;
-        for( int i=0;i< s.length()-k+1;i++){
-            string m= s.substr(i,k);
-            st.insert(m);
-        }
-        if( pow(2,k) == st.size())return true;
-        return false;
+        // set<string>st;
+        // if( s.length()<k)return false;
+        // for( int i=0;i< s.length()-k+1;i++){
+        //     string m= s.substr(i,k);
+        //     st.insert(m);
+        // }
+        // if( pow(2,k) == st.size())return true;
+        // return false;
+        int n= s.size;()
+         unordered_set<string>st;
+        for(int i =0;i<=n-k;i++) st.insert(s.substr(i,k));
+        return st.size() == (1<<k);
     }
 };
