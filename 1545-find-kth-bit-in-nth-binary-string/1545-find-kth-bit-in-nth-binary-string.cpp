0178d758268fc1ccf81ccc1e088c3794d9cfbde9
Harshavardhan kale <94823507+kaleharshavardhan07@users.noreply.github.com>
2026-03-03 18:05:08 +0530

    Time: 0 ms (100%), Space: 7.7 MB (97.44%) - LeetHub

--- 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.cpp
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.cpp
@@ -1,22 +1,48 @@
+// class Solution {
+// public:
+//     string inv( string k){
+//         string s;
+//            for( char c: k){
+//             if( c=='1'){
+//                 s=s+'0';
+//             }
+//             else{
+//                  s=s+'1';
+//             }
+//            }
+//            reverse(s.begin(),s.end());
+//            return s;
+//     }
+//     char findKthBit(int n, int k) {
+//         string s1="0";
+
+//         for( int i=0;i<n;i++){
+//             s1=s1+'1'+inv(s1);
+//         }
+
+//         return s1[k-1];
+        
+
+//     }
+// };
 class Solution {
 public:
+    char solve(int n, int k) {
+        if(n == 1) return '0';
+
+        int len = (1 << n) - 1;
+        int mid = (len / 2) + 1;
+
+        if(k == mid) return '1';
+
+        if(k < mid) 
+            return solve(n - 1, k);
+        
+        // mirror + invert
+        return solve(n - 1, len - k + 1) == '0' ? '1' : '0';
+    }
+
     char findKthBit(int n, int k) {
-    if (n == 1) {
-        return '0';
+        return solve(n, k);
     }
-    
-    int length = pow(2, n) - 1;
-    int mid = length / 2 + 1;
-    
-    if (k == mid) {
-        return '1';
-    }
-    
-    if (k < mid) {
-        return findKthBit(n - 1, k);
-    } else {
-        char invert_bit = findKthBit(n - 1, length - k + 1);
-        return (invert_bit == '0') ? '1' : '0';
-    }
-}
 };
